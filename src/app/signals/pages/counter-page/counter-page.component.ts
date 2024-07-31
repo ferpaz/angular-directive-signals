21d8ea3276ce80e60c7ec438e7c77c0981180692
Fernando Paz <ferpaz64@gmail.com>
2024-07-31 14:15:37 -0600

    Se creo un counter usando signal y readonly signal

--- src/app/signals/pages/counter-page/counter-page.component.ts
+++ src/app/signals/pages/counter-page/counter-page.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, computed, signal } from '@angular/core';
 
 @Component({
   selector: 'signals-counter-page',
@@ -7,4 +7,21 @@ import { Component } from '@angular/core';
 })
 export class CounterPageComponent {
 
+  // signal with initial value
+  public counter = signal(10);
+
+  // readonly signal
+  public squareCounter = computed(() => this.counter() ** 2);
+
+  public increment( value: number) {
+    this.counter.update( current => current + value);
+  }
+
+  public decrement( value: number) {
+    this.counter.update( current => current - value);
+  }
+
+  public reset() {
+    this.counter.set(0);
+  }
 }
